b17be0f0e6c0fe0adf8c35d9cb34b510d265df20
SamGG00 <samir.1701825098@ucaldas.edu.co>
2025-01-05 14:51:03 -0500

    Cambios footer colores tipografia y orden.

--- src/pages/home/components/Footer.jsx
+++ src/pages/home/components/Footer.jsx
@@ -6,26 +6,25 @@ export default function Footer() {
         <Box
             component="footer"
             sx={{
-                backgroundColor: "#2E8B57", // Color de fondo
+                backgroundColor: "#8FD5A6", // Color de fondo
                 color: "text.primary",      // Color de texto (tomado del tema)
                 mt: "auto",                 // "Empujar" el footer hacia abajo si usas un layout de altura completa
-                py: 1,                      // Padding vertical
+                py: 0.5,                      // Padding vertical
                 px: 3,                      // Padding horizontal
-                marginTop:2,
+                marginTop: 2,
             }}
         >
-
-
             {/* Línea de separación opcional */}
             <Box
                 sx={{
-                    borderTop: "1px solid #ccc",
-                    mt: 1,
-                    pt: 1,
+                    borderTop: "1px solid #2E8B57",
+                    mt: 0.5,
+                    pt: 0.5,
+                    pb: 1,
                     textAlign: "center",
                 }}
             >
-                <Typography variant="body2" sx={{fontWeight: 500 ,fontFamily : "Times New Roman, serif"} }>
+                <Typography variant="body2" sx={{ fontWeight: 500, fontFamily: "Times New Roman, serif", color: "white", marginTop: 1 }}>
                     © {new Date().getFullYear()} - Todos los derechos reservados
                 </Typography>
             </Box>
